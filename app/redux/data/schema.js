4b539ace73b6a885bc9440037714c5c7350be29f
Petr Marecek <petr.marecekk@gmail.com>
2018-01-08 11:35:38 +0100

    Fix error with normalizr

--- app/redux/data/schema.js
+++ app/redux/data/schema.js
@@ -1,35 +1,36 @@
 import { schema } from 'normalizr';
 
-const tag = new schema.Entity('tags', { idAttribute: 'id' })
+const tag = new schema.Entity('tags', {}, { idAttribute: 'id' })
 const tagList = new schema.Array(tag)
 
-const task = new schema.Entity('tasks', { idAttribute: 'id' })
+const task = new schema.Entity('tasks', {}, { idAttribute: 'id' })
 const taskList = new schema.Array(task)
 
 task.define({
   tags: new schema.Array(tag),
 })
 
-const treeItem = new schema.Entity('treeItem', { idAttribute: 'id' })
+const treeItem = new schema.Entity('treeItem', {}, { idAttribute: 'id' })
+
 treeItem.define({
   tag
 })
 
-const tree = new schema.Entity('tree', { idAttribute: 'parentId' })
+const tree = new schema.Entity('tree', {}, { idAttribute: 'parentId' })
 const treeList = new schema.Array(tree)
 
 tree.define({
   items: new schema.Array(treeItem)
 })
 
-const comment = new schema.Entity('comment', { idAttribute: 'id' })
+const comment = new schema.Entity('comment', {}, { idAttribute: 'id' })
 const commentList = new schema.Array(comment)
 
 comment.define({
   items: new schema.Array(comment)
 })
 
-const attachment = new schema.Entity('attachment', { idAttribute: 'id' })
+const attachment = new schema.Entity('attachment', {}, { idAttribute: 'id' })
 const attachmentList = new schema.Array(attachment)
 
 attachment.define({
