fe79caa5f72f149f7c12741f155a88204e4bbabd
Petr Marecek <petr.marecekk@gmail.com>
2018-03-12 17:02:34 +0100

    Fix problem with scroll to top/bottom of list if user drags some element in list

--- app/components/elements/shadow-scrollbar.js
+++ app/components/elements/shadow-scrollbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { findDOMNode } from 'react-dom'
 import { Scrollbars } from 'react-custom-scrollbars'
 
 export default class ShadowScrollbar extends Component {
@@ -25,18 +26,20 @@ export default class ShadowScrollbar extends Component {
         <Scrollbars
           ref="scrollbars"
           renderThumbVertical={scrollProps => <div {...scrollProps} style={verticalStyle} />}
+          onDrag={this.handleDrag}
           onUpdate={this.handleUpdate}
           {...props}/>
       )
       : (
         <Scrollbars
           ref="scrollbars"
+          onDrag={this.handleDrag}
           onUpdate={this.handleUpdate}
           {...props}/>
       )
   }
 
-  handleUpdate = (values) => {
+  handleUpdate = values => {
     const { shadowTop, shadowBottom } = this.refs
     const { scrollTop, scrollHeight, clientHeight } = values
     const shadowTopOpacity = 1 / 20 * Math.min(scrollTop, 20)
@@ -44,6 +47,27 @@ export default class ShadowScrollbar extends Component {
     const shadowBottomOpacity = 1 / 20 * (bottomScrollTop - Math.max(scrollTop, bottomScrollTop - 20))
     shadowTop.style.opacity = shadowTopOpacity
     shadowBottom.style.opacity = shadowBottomOpacity
+  }
+
+  handleDrag = values => {
+    const { scrollbars } = this.refs
+    // Position of tasks list
+    const { left, top, right, bottom } = findDOMNode(scrollbars).getBoundingClientRect()
+    // Current position of mouse
+    const { clientX, clientY } = values
+    const conditionX = clientX >= left && clientX <= right
+    const conditionTopY = clientY >= top && clientY <= (top + 30)
+    const conditionBottomY = clientY >= (bottom - 30) && clientY <= bottom
+
+    // Scroll to top
+    if (conditionX && conditionTopY) {
+        scrollbars.view.scrollTop -= 10
+    }
+
+    // Scroll to bottom
+    if (conditionX && conditionBottomY) {
+      scrollbars.view.scrollTop += 10
+    }
   }
 
   render() {
