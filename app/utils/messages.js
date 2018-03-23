5b3133ce8b39b0d51c697e8220a39fae6884d4c0
Petr Marecek <petr.marecekk@gmail.com>
2018-03-23 11:09:18 +0100

    Edit notification messages

--- app/utils/messages.js
+++ app/utils/messages.js
@@ -1,12 +1,12 @@
 export const errorMessages = {
 
   signIn: {
-    unauthorized: 'Incorrect E-mail or Password',
-    passwordResetRequired: 'Please, reset your password. Click on the Forgot your password?',
+    unauthorized: 'Incorrect E-mail or Password.',
+    passwordResetRequired: 'Please, reset your password. Click on forgot your password?',
   },
 
   signUp: {
-    conflict: 'This E-mail is already in use.',
+    conflict: 'This E-mail has already been used.',
   },
 
   changePassword: {
@@ -14,21 +14,21 @@ export const errorMessages = {
   },
 
   resetPassword: {
-    linkExpired: 'Link is expired. Please ask for a new one.',
+    linkExpired: 'Link has expired. Please ask for a new one.',
   },
 
   tags: {
-    createConflict: 'Cannot create tag.',
+    createConflict: 'This tag has already been created.',
     referenceDeleteConflict: 'The target tag cannot be deleted because it is referenced ' +
     'in the filter tree. Please delete the referencing tree item first.',
-    relationDeleteConflict: 'The target tag cannot be deleted because it has ' +
-    'relations in tasks list. Please delete the relations first.',
+    relationDeleteConflict: 'The target tag cannot be deleted because it has relations ' +
+    'in tasks list. Please delete the relations first.',
   },
 
   treeItems: {
-    duplicatePathConflict: 'Cannot perform this move tree validation failed. ' +
+    duplicatePathConflict: 'Cannot perform this move. Tree validation failed. ' +
     'Tag duplication found in the result tree path.',
-    duplicateLevelConflict: 'Cannot perform this move tree validation failed. ' +
+    duplicateLevelConflict: 'Cannot perform this move. Tree validation failed. ' +
     'The same tag found on the same tree level.',
   },
 
@@ -41,12 +41,12 @@ export const successMessages = {
   },
 
   tasks: {
-    archive: 'Task was archived.',
-    cancelArchive: 'Task was returned to the main list as completed task.',
+    archive: 'Task has been archived.',
+    cancelArchive: 'Task has been returned to the main list as completed task.',
   },
 
   multiSelect: {
-    archive: 'Tasks were archived.',
+    archive: 'Tasks have been archived.',
   },
 
   changePassword: 'Password has successfully been changed.',
