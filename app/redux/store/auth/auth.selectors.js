96031b1f6cc1728749f263fddc87928b6d431a8a
Petr Marecek <petr.marecekk@gmail.com>
2018-05-29 20:02:01 +0200

    Implement reselect for entities selectors

--- app/redux/store/auth/auth.selectors.js
+++ app/redux/store/auth/auth.selectors.js
@@ -1,5 +1,15 @@
-export const getAuth = (state) => {
-  return state.getIn(['auth'])
+// ------ Selectors -------------------------------------------------------------
+
+// Export selectors
+export const getAuth = state => state.getIn(['auth'])
+export const getNewRefreshToken = state => state.getIn(['auth', 'newRefreshToken'])
+
+export const getUserId = state => {
+  if (!state.getIn(['auth', 'profile'])) {
+    return null
+  }
+
+  return state.getIn(['auth', 'profile', 'id'])
 }
 
 export const getEmail = (state) => {
@@ -8,12 +18,4 @@ export const getEmail = (state) => {
   }
 
   return state.getIn(['auth', 'profile', 'email'])
-}
-
-export const getNewRefreshToken = (state) => {
-  return state.getIn(['auth', 'newRefreshToken'])
-}
-
-export const getUserId = (state) => {
-  return state.getIn(['auth', 'profile', 'id'])
 }
--- app/redux/store/entities/entities.selectors.js
+++ app/redux/store/entities/entities.selectors.js
@@ -1,18 +1,22 @@
+import { createSelector } from 'reselect'
 
+// ------ Selectors -------------------------------------------------------------
+
+// Export selectors
 export const getEntitiesTasks = state => state.getIn(['entities', 'tasks'])
-
 export const getEntitiesTags = state => state.getIn(['entities', 'tags'])
-
 export const getEntitiesTreeItems = state => state.getIn(['entities', 'treeItems'])
-
 export const getEntitiesAttachments = state => state.getIn(['entities', 'attachments'])
-
 export const getEntitiesComments = state => state.getIn(['entities', 'comments'])
 
-export const getActiveEntitiesTasks = state => {
-  const tasks = state.getIn(['entities', 'tasks'])
+// ------ Reselect selectors ----------------------------------------------------
 
-  return tasks
-    .filter(task => task && task.isTrashed === false)
-    .filter(task => task && task.isArchived === false)
-}
+export const getActiveEntitiesTasks = createSelector(
+  getEntitiesTasks,
+  (entitiesTasks) => {
+
+    return entitiesTasks
+      .filter(task => task && task.isTrashed === false)
+      .filter(task => task && task.isArchived === false)
+  }
+)
