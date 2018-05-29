68ce2a2e8bbc813d609693b9f5205136881b22e9
Petr Marecek <petr.marecekk@gmail.com>
2018-05-29 13:50:38 +0200

    Implement reselect for app-state selectors

--- app/redux/store/app-state/app-state.selectors.js
+++ app/redux/store/app-state/app-state.selectors.js
@@ -1,67 +1,36 @@
+import { getEntitiesTags } from '../entities/entities.selectors'
+import { createSelector } from 'reselect'
 
+// ------ Selectors ----------------------------------------------------
+
+// Local selectors
+const getTagHintsVisibleTags = state => state.getIn(['appState', 'tagHints', 'visibleTags'])
+
+// Export selectors
 export const getAppState = state => state.getIn(['appState'])
-
 export const getLeftPanel = state => state.getIn(['appState', 'leftPanel'])
-
-export const getTagHints = state => {
-  const tagHintsData = state.getIn(['appState', 'tagHints'])
-  const visibleTags = state
-    .getIn(['appState', 'tagHints', 'visibleTags'])
-    .map(tagId => state.getIn(['entities', 'tags', tagId]))
-
-  return tagHintsData.setIn(['visibleTags'], visibleTags)
-}
-
 export const getTagHintsRaw = state => state.getIn(['appState', 'tagHints'])
-
 export const getTagHintsVisibility = state => state.getIn(['appState', 'tagHints', 'isVisible'])
-
 export const getTagHintsSelectOnly = state => state.getIn(['appState', 'tagHints', 'isSelectOnly'])
-
 export const getTagAutocompletes = state => state.getIn(['appState', 'tagAutocompletes'])
+export const getAppStateItem = (state, type) => state.getIn(['appState', type])
+export const getMultiSelectVisibility = state => state.getIn(['appState', 'multiSelect', 'isVisible'])
+export const getCurrentDialog = state => state.getIn(['appState', 'currentDialog'])
+export const getArchivedTasksVisibility = state => state.getIn(['appState', 'archivedTasks', 'isVisible'])
+export const getTaskTagDetail = state => state.getIn(['appState', 'taskTagDetail'])
+export const getTaskDetail = state => state.getIn(['appState', 'taskTagDetail', 'task'])
+export const getTaskArchiveDetail = state => state.getIn(['appState', 'taskTagDetail', 'archive'])
+export const getLoader = state => state.getIn(['appState', 'loader', 'isVisible'])
 
-export const getAppStateItem = (state, type) => {
-  return state.getIn(['appState', type])
-}
+// ------ Reselect selectors ----------------------------------------------------
 
-export const getMultiSelectVisibility = (state) => {
-  return state.getIn(['appState', 'multiSelect', 'isVisible'])
-}
+export const getTagHints = createSelector(
+  getTagHintsRaw,
+  getTagHintsVisibleTags,
+  getEntitiesTags,
+  (tagHintsRaw, tagHintsVisibleTags, entitiesTags) => {
+    const visibleTags = tagHintsVisibleTags.map(tagId => entitiesTags.getIn([tagId]))
 
-export const getDialog = (state) => {
-  const dialog = state.getIn(['appState', 'currentDialog'])
-
-  if (!dialog) {
-    return false
+    return tagHintsRaw.setIn(['visibleTags'], visibleTags)
   }
-
-  return true
-}
-
-export const getCurrentDialog = state => {
-  return state.getIn(['appState', 'currentDialog'])
-}
-
-export const getTasksListVisibility = state => {
-  return state.getIn(['appState', 'tasksList', 'isVisible'])
-}
-
-export const getArchivedTasksVisibility = state => {
-  return state.getIn(['appState', 'archivedTasks', 'isVisible'])
-}
-
-export const getTaskTagDetail = state => {
-  return state.getIn(['appState', 'taskTagDetail'])
-}
-
-export const getTaskDetail = state => {
-  return state.getIn(['appState', 'taskTagDetail', 'task'])
-}
-
-export const getTaskArchiveDetail = state => {
-  return state.getIn(['appState', 'taskTagDetail', 'archive'])
-}
-
-export const getLoader = state => {
-  return state.getIn(['appState', 'loader', 'isVisible'])
-}
+)
