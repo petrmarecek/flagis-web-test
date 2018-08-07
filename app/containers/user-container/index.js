3e67982a6e9d030cef6d952e61854bb1af4ee6f0
Petr Marecek <petr.marecekk@gmail.com>
2018-08-07 12:59:51 +0200

    feat(clean-code): Remove archive-page and contact-page from user-container

--- app/containers/user-container/index.js
+++ app/containers/user-container/index.js
@@ -15,8 +15,6 @@ import NavigationBar from 'components/navigation/navigation-bar'
 import FloatingComponents from 'components/floating/floating-components'
 import TaskPage from 'containers/task-page'
 import TagPage from 'containers/tag-page'
-import ArchivePage from 'containers/archive-page'
-import ContactPage from 'containers/contact-page'
 import AccountContainer from 'containers/account-container'
 
 class UserContainer extends PureComponent {
@@ -68,8 +66,6 @@ class UserContainer extends PureComponent {
           <Switch>
             <Route path={`${this.props.match.path}/tasks`} component={TaskPage} />
             <Route path={`${this.props.match.path}/tags`} component={TagPage} />
-            <Route path={`${this.props.match.path}/archive`} component={ArchivePage} />
-            <Route path={`${this.props.match.path}/contacts`} component={ContactPage} />
             <Route path={`${this.props.match.path}/account`} component={AccountContainer} />
           </Switch>
         </div>
