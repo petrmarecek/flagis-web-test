8e9466d0cf060798e394b205026d021a7b1cce90
Petr Marecek <petr.marecekk@gmail.com>
2018-09-25 16:55:06 +0200

    feat(search-box): Rewrite to HOC pattern

--- app/components/common/search-box.js
+++ app/components/common/search-box.js
@@ -1,47 +1,76 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import { withHandlers } from 'recompose'
 
 import { ICONS } from 'components/icons/icon-constants'
 import Icon from 'components/icons/icon'
 
-export default class SearchBox extends PureComponent {
+import styled from 'styled-components'
+import { placeholderColor, boxSizing, fontSub } from '../styled-components-mixins'
 
-  static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    value: PropTypes.string,
-  }
+const Search = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
-  handleUpdate = event => {
-    const value = event.target.value
-    this.props.onChange(value)
-  }
+const SearchIcon = styled(Icon)`
+  margin-left: 10px;
+  flex-shrink: 0;
+`;
 
-  handleKeyUp = event => {
+const Input = styled.input`
+  ${placeholderColor('#8c9da9')}
+  ${boxSizing('border-box')}
+  ${fontSub}
+  font-size: 14px;
+  margin: 0 0 0 12px;
+  border: none;
+  height: 26px;
+  color: #293034;
+  width: 100%;
+  background-color: transparent;
+`;
+
+const SearchBox = ({ value, handleUpdate, handleKeyUp }) => (
+  <Search>
+    <SearchIcon
+      icon={ICONS.MAGNIFIER}
+      width={17}
+      height={18}
+      color={["#8C9DA9"]} />
+    <Input
+      id="search"
+      type="text"
+      name="search"
+      placeholder='Full text search'
+      autoComplete="off"
+      value={value}
+      onChange={handleUpdate}
+      onKeyUp={handleKeyUp} />
+  </Search>
+)
+
+SearchBox.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  handleUpdate: PropTypes.func,
+  handleKeyUp: PropTypes.func,
+}
+
+export default withHandlers({
+  handleUpdate: props => event => props.onChange(event.target.value),
+  handleKeyUp: props => event => {
     switch (event.which) {
+      // escape
       case 27:
-        this.props.onChange('')
-        return;
+        props.onChange('')
+        return
+
       default:
-        // intentionally no action
         return
     }
   }
-
-  render() {
-    return (
-      <div className="search">
-        <Icon
-          className="search__icon"
-          icon={ICONS.MAGNIFIER}
-          width={17}
-          height={18}
-          color={["#8C9DA9"]}/>
-        <input id="search" type="text" name="search" className="search__input"
-          placeholder='Full text search' autoComplete="off"
-          value={this.props.value}
-          onChange={this.handleUpdate}
-          onKeyUp={this.handleKeyUp} />
-      </div>
-    )
-  }
-}
+})(SearchBox)
