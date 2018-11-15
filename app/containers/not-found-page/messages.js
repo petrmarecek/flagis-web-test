d056ff2feb46e3bd855e27e08db14b3c638fae26
Petr Marecek <petr.marecekk@gmail.com>
2018-11-15 17:14:23 +0100

    fix(tests): Fix error with test for not-found-page

--- app/containers/not-found-page/messages.js
+++ app/containers/not-found-page/messages.js
@@ -1,17 +1,8 @@
-/*
- * NotFoundPage Messages
- *
- * This contains all the text for the NotFoundPage component.
- */
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  title: {
-    id: 'app.components.NotFoundPage.title',
-    defaultMessage: 'Oops!',
-  },
-  text: {
-    id: 'app.components.NotFoundPage.text',
-    defaultMessage: 'It looks like you\'re lost.',
-  },
+  header: {
+    id: 'app.components.NotFoundPage.header',
+    defaultMessage: 'This is NotFoundPage component!',
+  }
 });
