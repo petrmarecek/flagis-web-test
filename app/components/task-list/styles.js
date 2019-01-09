45edaa0543657bc4f225e0992a45e9e3a0fa1640
Petr <petr.marecekk@gmail.com>
2019-01-09 14:53:18 +0100

    feat(task-list): Rewrite to HOC pattern

--- app/components/task-list/styles.js
+++ app/components/task-list/styles.js
@@ -7,13 +7,73 @@ import {
   textOverflow,
   borderRadius,
   fontMain,
-  link
+  link,
 } from '../styled-components-mixins'
 
-/*----------------------------------- Task Item ----------------------------------------------*/
+/*----------------------------------- Task List ----------------------------------------------*/
+const TaskListItems = styled.ul`
+  clear: both;
 
-const showAnimation = keyframes`${fadeInUp}`;
-const hideAnimation = keyframes`${flipOutX}`;
+  li {
+    list-style-type: none;
+  }
+`
+
+const TimeLine = styled.ul`
+  ${fontMain}
+  padding-left: 25px;
+  position: relative;
+
+  &:before {
+    content: '';
+    width: 2px;
+    height: 100%;
+    background: #fff;
+    left: 11px;
+    top: 0;
+    position: absolute;
+  }
+
+  &:after {
+    content: '';
+    width: 2px;
+    height: 8px;
+    background: #e8ecee;
+    left: 11px;
+    top: 0;
+    position: absolute;
+  }
+`
+
+const TimeLineList = styled.li`
+  position: relative;
+`
+
+const TimeLinePoint = styled.span`
+  &:before {
+    content: '';
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    position: absolute;
+    background: #fff;
+    left: -18px;
+    top: 8px;
+  }
+`
+
+const TimeLineText = styled.p`
+  padding: 5px 0 5px 0;
+  font-size: 14px;
+  font-weight: 600;
+  text-align: left;
+  color: #8c9ea9;
+`
+
+/*----------------------------------- Task List Item ----------------------------------------------*/
+
+const showAnimation = keyframes`${fadeInUp}`
+const hideAnimation = keyframes`${flipOutX}`
 
 const TaskItem = styled.div`
   height: 50px;
@@ -24,8 +84,9 @@ const TaskItem = styled.div`
   position: relative;
   outline: none;
   background-color: ${props => props.backgroundColor};
-  visibility: ${props => props.dragging ? 'hidden' : 'visible'};
-  animation: ${props => props.isMounted ? `${showAnimation} 400ms` : `${hideAnimation} 400ms`};
+  visibility: ${props => (props.dragging ? 'hidden' : 'visible')};
+  animation: ${props =>
+    props.isMounted ? `${showAnimation} 400ms` : `${hideAnimation} 400ms`};
 
   :before {
     content: "";
@@ -35,12 +96,12 @@ const TaskItem = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: ${props => props.selected ? '#ffffd7' : '#ecfff7'};
-    ${transform(props => props.completed ? 'scaleX(1)' : 'scaleX(0)')}
+    background-color: ${props => (props.selected ? '#ffffd7' : '#ecfff7')};
+    ${transform(props => (props.completed ? 'scaleX(1)' : 'scaleX(0)'))}
     ${transformOrigin('0 50%')}
     ${transition('transform 500ms ease-out')}
   }
-`;
+`
 
 const Completed = styled.div`
   display: flex;
@@ -57,11 +118,11 @@ const Completed = styled.div`
   :hover {
     svg {
       path {
-        fill: #00FFC7;
+        fill: #00ffc7;
       }
     }
   }
-`;
+`
 
 const Archived = styled.div`
   display: flex;
@@ -70,67 +131,68 @@ const Archived = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: ${props => props.archived ? '0' : '45px'};
+  left: ${props => (props.archived ? '0' : '45px')};
   width: 45px;
-  padding-left: ${props => props.archived ? '13px' : '6px'};
+  padding-left: ${props => (props.archived ? '13px' : '6px')};
   z-index: 1;
 
   :hover {
     svg {
       path {
-        fill: ${props => props.archived ? '#8c9ea9' : '#282f34'};
+        fill: ${props => (props.archived ? '#8c9ea9' : '#282f34')};
       }
     }
   }
-`;
+`
 
 const FollowerResponse = styled.div`
   position: absolute;
   top: 12px;
   left: 11px;
-`;
+`
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: ${props => props.marginLeft};
-  margin-right: ${props => props.followers ? '70px' : '15px'};
+  margin-right: ${props => (props.followers ? '70px' : '15px')};
   ${transition('margin 500ms ease-out')};
-`;
+`
 
-const SubjectTags =  styled.div`
+const SubjectTags = styled.div`
   display: flex;
-`;
+`
 
-const Subject =  styled.div`
+const Subject = styled.div`
   ${textOverflow('ellipsis')}
   flex: auto;
   max-width: 70%;
-  margin: ${props => props.description ? '5px 0' : '14px 0 0 0'};
+  margin: ${props => (props.description ? '5px 0' : '14px 0 0 0')};
   height: 23px;
   line-height: 23px;
   font-size: 18px;
   overflow: hidden;
   white-space: nowrap;
   z-index: 1;
-  color: ${props => props.completed ? '#cfdbe4' : '#293034'};
-  font-weight: ${props => props.important ? 'bold' : 'normal'};
-  text-decoration: ${props => (props.completed || props.archived) ? 'line-through' : 'none'};
+  color: ${props => (props.completed ? '#cfdbe4' : '#293034')};
+  font-weight: ${props => (props.important ? 'bold' : 'normal')};
+  text-decoration: ${props =>
+    props.completed || props.archived ? 'line-through' : 'none'};
   ${transition('color 500ms ease-out')};
   ${link}
-`;
+`
 
-const Tags =  styled.div`
+const Tags = styled.div`
   flex: auto;
   margin: 5px 0 0 5px;
   z-index: 1;
-`;
+`
 
-const DescriptionDueDate =  styled.div`
+const DescriptionDueDate = styled.div`
   display: flex;
-`;
+`
 
-const Description =  styled.div`
+const Description = styled.div`
   ${textOverflow('ellipsis')}
   flex: 10 10 auto;
   font-size: 14px;
@@ -139,25 +201,26 @@ const Description =  styled.div`
   max-height: 18px;
   white-space: nowrap;
   overflow: hidden;
-  color: ${props => props.completed ? '#cfdbe4' : '#8C9DA9'};
+  color: ${props => (props.completed ? '#cfdbe4' : '#8C9DA9')};
   margin: -5px 0 5px 0;
   z-index: 1;
-`;
+`
 
-const DueDate =  styled.div`
+const DueDate = styled.div`
   flex: 10 0 60px;
   height: 18px;
   line-height: 18px;
   right: 18px;
   font-size: 12px;
-  color: ${props => props.overdue && !props.completed ? '#ff6a6a' : '#8C9DA9'};
-  font-weight: ${props => props.overdue && !props.completed ? 600 : 'normal'};
+  color: ${props =>
+    props.overdue && !props.completed ? '#ff6a6a' : '#8C9DA9'};
+  font-weight: ${props => (props.overdue && !props.completed ? 600 : 'normal')};
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  margin: ${props => props.description ? '-4px 8px 0 0' : '-7px 8px 0 0'};
+  margin: ${props => (props.description ? '-4px 8px 0 0' : '-7px 8px 0 0')};
   z-index: 1;
-`;
+`
 
 const Followers = styled.div`
   display: flex;
@@ -169,15 +232,15 @@ const Followers = styled.div`
   right: 0;
   top: 0;
   z-index: 1;
-`;
+`
 
 /*----------------------------------- Tag Items ----------------------------------------------*/
 
-const TagItems =  styled.ul`
+const TagItems = styled.ul`
   display: flex;
   list-style-type: none;
   justify-content: flex-end;
-`;
+`
 
 const Item = styled.li`
   ${borderRadius('12px')}
@@ -189,9 +252,9 @@ const Item = styled.li`
   float: left;
   height: 23px;
   background-color: ${props => props.bgColor};
-  max-width: ${props => props.isItemCollapse ? '23px' : '120px'};
+  max-width: ${props => (props.isItemCollapse ? '23px' : '120px')};
   ${transition('500ms')};
-`;
+`
 
 const Text = styled.div`
   ${textOverflow('ellipsis')}
@@ -200,11 +263,16 @@ const Text = styled.div`
   line-height: 23px;
   overflow: hidden;
   white-space: nowrap;
-  color: ${props => props.isItemCollapse ? props.bgColor : '#fff' };
+  color: ${props => (props.isItemCollapse ? props.bgColor : '#fff')};
   ${transition('250ms')};
-`;
+`
 
 export {
+  TaskListItems,
+  TimeLine,
+  TimeLineList,
+  TimeLinePoint,
+  TimeLineText,
   TaskItem,
   Completed,
   Archived,
@@ -217,8 +285,7 @@ export {
   Description,
   DueDate,
   Followers,
-
   TagItems,
   Item,
-  Text
+  Text,
 }
--- app/components/task-list/task-list.js
+++ app/components/task-list/task-list.js
@@ -1,49 +1,54 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { List } from 'immutable'
-import TaskListItem from 'components/task-list/task-list-item'
 import moment from 'moment'
+import { List } from 'immutable'
+
+import TaskListItem from 'components/task-list/task-list-item'
+
 import { getTimeLineByDueDate } from 'redux/utils/component-helper'
 
-export default class TaskList extends PureComponent {
+import {
+  TaskListItems,
+  TimeLine,
+  TimeLineList,
+  TimeLinePoint,
+  TimeLineText,
+} from './styles'
 
-  static propTypes = {
-    // Data
-    userId: PropTypes.string,
-    tasks: PropTypes.array,
-    listType: PropTypes.string,
-    selectedTasks: PropTypes.object,
-    selectedTags: PropTypes.object,
-    isVisibleArchivedTasks: PropTypes.bool,
-    timeLine: PropTypes.bool,
-    sort: PropTypes.object,
-    leftPanelWidth: PropTypes.number,
-    windowWidth: PropTypes.number,
+const TaskList = props => {
+  const {
+    // data
+    userId,
+    tasks,
+    listType,
+    selectedTasks,
+    selectedTags,
+    isVisibleArchivedTasks,
+    timeLine,
+    sort,
+    leftPanelWidth,
+    windowWidth,
 
-    // Handlers
-    onCompleteClick: PropTypes.func,
-    onTaskSelect: PropTypes.func,
-    moveTask: PropTypes.func,
-    dropTask: PropTypes.func,
-    onTagClick: PropTypes.func,
-    setArchiveTasks: PropTypes.func,
-    cancelArchiveTasks: PropTypes.func,
-    acceptTask: PropTypes.func,
-    rejectTask: PropTypes.func,
-  }
+    // handlers
+    onCompleteClick,
+    onTaskSelect,
+    moveTask,
+    dropTask,
+    onTagClick,
+    setArchiveTasks,
+    cancelArchiveTasks,
+    acceptTask,
+    rejectTask,
+  } = props
 
-  isSelected(taskId) {
-    return this.props.selectedTasks.has(taskId)
-  }
-
-  getEmptyTask(typeDate, section) {
+  const getEmptyTask = (typeDate, section) => {
     const now = moment()
     const dayOfWeek = now.isoWeekday()
-    const daysToNewWeek = (7 - dayOfWeek) + 1
+    const daysToNewWeek = 7 - dayOfWeek + 1
     const dayOfMonth = now.date()
     const daysOfMonth = now.daysInMonth()
     const add = dayOfMonth === daysOfMonth ? 2 : 1
-    const daysToNewMonth = (daysOfMonth - dayOfMonth) + add
+    const daysToNewMonth = daysOfMonth - dayOfMonth + add
     const date = {
       today: now,
       tomorrow: now.clone().add(1, 'days'),
@@ -63,113 +68,51 @@ export default class TaskList extends PureComponent {
         index={0}
         listType="main"
         task={task}
-        selectedTags={this.props.selectedTags}
-        moveTask={this.props.moveTask}
-        dropTask={this.props.dropTask}
+        selectedTags={selectedTags}
+        moveTask={moveTask}
+        dropTask={dropTask}
         timeLine={null}
         sort={null}
         section={section}
-        noTaskFound/>
+        noTaskFound
+      />
     )
   }
 
-  getTaskItems(tasks, section) {
-    const { listType } = this.props
-
+  const getTaskItems = (taskItems, section) => {
     if (!listType) {
       return null
     }
 
-    switch (listType) {
-      case 'main': {
-        return (
-          tasks.map((task, i) => (
-            <TaskListItem
-              key={task.id}
-              userId={this.props.userId}
-              task={task}
-              index={i}
-              order={task.order}
-              listType={this.props.listType}
-              isSelected={this.isSelected(task.id)}
-              selectedTags={this.props.selectedTags}
-              onCompleteClick={this.props.onCompleteClick}
-              onClick={this.props.onTaskSelect}
-              moveTask={this.props.moveTask}
-              dropTask={this.props.dropTask}
-              onTagClick={this.props.onTagClick}
-              timeLine={this.props.timeLine}
-              sort={this.props.sort}
-              setArchiveTasks={this.props.setArchiveTasks}
-              section={section}
-              leftPanelWidth={this.props.leftPanelWidth}
-              windowWidth={this.props.windowWidth} />
-          ))
-        )
-      }
-
-      case 'archived': {
-        return (
-          tasks.map((task, i) => (
-            <TaskListItem
-              key={task.id}
-              userId={this.props.userId}
-              task={task}
-              index={i}
-              order={task.order}
-              listType={this.props.listType}
-              isSelected={this.isSelected(task.id)}
-              selectedTags={this.props.selectedTags}
-              onClick={this.props.onTaskSelect}
-              moveTask={this.props.moveTask}
-              dropTask={this.props.dropTask}
-              onTagClick={this.props.onTagClick}
-              timeLine={this.props.timeLine}
-              sort={this.props.sort}
-              cancelArchiveTasks={this.props.cancelArchiveTasks}
-              section={section}
-              leftPanelWidth={this.props.leftPanelWidth}
-              windowWidth={this.props.windowWidth} />
-          ))
-        )
-      }
-
-      case 'inbox': {
-        return (
-          tasks.map((task, i) => (
-            <TaskListItem
-              key={task.id}
-              task={task}
-              userId={this.props.userId}
-              index={i}
-              order={task.order}
-              listType={this.props.listType}
-              isSelected={this.isSelected(task.id)}
-              selectedTags={this.props.selectedTags}
-              onCompleteClick={this.props.onCompleteClick}
-              onClick={this.props.onTaskSelect}
-              moveTask={this.props.moveTask}
-              dropTask={this.props.dropTask}
-              onTagClick={this.props.onTagClick}
-              timeLine={this.props.timeLine}
-              sort={this.props.sort}
-              setArchiveTasks={this.props.setArchiveTasks}
-              acceptTask={this.props.acceptTask}
-              rejectTask={this.props.rejectTask}
-              section={section}
-              leftPanelWidth={this.props.leftPanelWidth}
-              windowWidth={this.props.windowWidth} />
-          ))
-        )
-      }
-
-      default: return null
-    }
+    return taskItems.map((task, i) => (
+      <TaskListItem
+        key={task.id}
+        userId={userId}
+        task={task}
+        index={i}
+        order={task.order}
+        listType={listType}
+        isSelected={selectedTasks.has(task.id)}
+        selectedTags={selectedTags}
+        onCompleteClick={onCompleteClick}
+        onClick={onTaskSelect}
+        moveTask={moveTask}
+        dropTask={dropTask}
+        onTagClick={onTagClick}
+        timeLine={timeLine}
+        sort={sort}
+        setArchiveTasks={setArchiveTasks}
+        cancelArchiveTasks={cancelArchiveTasks}
+        acceptTask={acceptTask}
+        rejectTask={rejectTask}
+        section={section}
+        leftPanelWidth={leftPanelWidth}
+        windowWidth={windowWidth}
+      />
+    ))
   }
 
-  getTaskItemsList(tasks) {
-    const { isVisibleArchivedTasks, timeLine } = this.props
-
+  const getTaskItemsList = () => {
     if (timeLine && !isVisibleArchivedTasks) {
       // Due date sorting algorithm is activated
       const timeLineTasks = getTimeLineByDueDate(tasks)
@@ -184,31 +127,52 @@ export default class TaskList extends PureComponent {
       }
 
       if (timeLineTasks.previousDaysTasks.length !== 0) {
-        tasksRender.previousDaysTasks = this.getTaskItems(timeLineTasks.previousDaysTasks, 'previousDaysTasks')
+        tasksRender.previousDaysTasks = getTaskItems(
+          timeLineTasks.previousDaysTasks,
+          'previousDaysTasks'
+        )
       }
 
       if (timeLineTasks.todayTasks.length !== 0) {
-        tasksRender.todayTasks = this.getTaskItems(timeLineTasks.todayTasks, 'todayTasks')
+        tasksRender.todayTasks = getTaskItems(
+          timeLineTasks.todayTasks,
+          'todayTasks'
+        )
       }
 
       if (timeLineTasks.tomorrowTasks.length !== 0) {
-        tasksRender.tomorrowTasks = this.getTaskItems(timeLineTasks.tomorrowTasks, 'tomorrowTasks')
+        tasksRender.tomorrowTasks = getTaskItems(
+          timeLineTasks.tomorrowTasks,
+          'tomorrowTasks'
+        )
       }
 
       if (timeLineTasks.weekTasks.length !== 0) {
-        tasksRender.weekTasks = this.getTaskItems(timeLineTasks.weekTasks, 'weekTasks')
+        tasksRender.weekTasks = getTaskItems(
+          timeLineTasks.weekTasks,
+          'weekTasks'
+        )
       }
 
       if (timeLineTasks.monthTasks.length !== 0) {
-        tasksRender.monthTasks = this.getTaskItems(timeLineTasks.monthTasks, 'monthTasks')
+        tasksRender.monthTasks = getTaskItems(
+          timeLineTasks.monthTasks,
+          'monthTasks'
+        )
       }
 
       if (timeLineTasks.laterTasks.length !== 0) {
-        tasksRender.laterTasks = this.getTaskItems(timeLineTasks.laterTasks, 'laterTasks')
+        tasksRender.laterTasks = getTaskItems(
+          timeLineTasks.laterTasks,
+          'laterTasks'
+        )
       }
 
       if (timeLineTasks.noDueDatesTasks.length !== 0) {
-        tasksRender.noDueDatesTasks = this.getTaskItems(timeLineTasks.noDueDatesTasks, 'noDueDatesTasks')
+        tasksRender.noDueDatesTasks = getTaskItems(
+          timeLineTasks.noDueDatesTasks,
+          'noDueDatesTasks'
+        )
       }
 
       return {
@@ -222,88 +186,114 @@ export default class TaskList extends PureComponent {
       }
     } else {
       // Due date sorting algorithm isn't activated
-      return this.getTaskItems(tasks, null)
+      return getTaskItems(tasks, null)
     }
   }
 
-  render() {
-    // children items
-    const { tasks, isVisibleArchivedTasks, timeLine } = this.props
-    const taskItems = this.getTaskItemsList(tasks)
+  // children items
+  const taskItems = getTaskItemsList()
+  return (
+    <div className="task-list-items">
+      {!timeLine && <TaskListItems>{taskItems}</TaskListItems>}
 
-    return (
-      <div className="task-list-items">
-        {isVisibleArchivedTasks &&
-        <ul ref="list" className="task-items">
-          {taskItems}
-        </ul>}
+      {timeLine && !isVisibleArchivedTasks && (
+        <TimeLine>
+          {taskItems.previousDaysTasks && (
+            <TimeLineList>
+              <TimeLinePoint />
+              <TimeLineText>Previous days</TimeLineText>
+              <TaskListItems>{taskItems.previousDaysTasks}</TaskListItems>
+            </TimeLineList>
+          )}
 
-        {!timeLine && !isVisibleArchivedTasks &&
-        <ul ref="list" className="task-items">
-          {taskItems}
-        </ul>}
+          <TimeLineList>
+            <TimeLinePoint />
+            <TimeLineText>Today</TimeLineText>
+            <TaskListItems>
+              {!taskItems.todayTasks
+                ? getEmptyTask('today', 'todayTasks')
+                : taskItems.todayTasks}
+            </TaskListItems>
+          </TimeLineList>
 
-        {timeLine && !isVisibleArchivedTasks &&
-        <ul className="time-line">
-          {taskItems.previousDaysTasks &&
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">Previous days</p>
-            <ul ref="list" className="task-items">
-              {taskItems.previousDaysTasks}
-            </ul>
-          </li>}
+          <TimeLineList>
+            <TimeLinePoint />
+            <TimeLineText>Tomorrow</TimeLineText>
+            <TaskListItems>
+              {!taskItems.tomorrowTasks
+                ? getEmptyTask('tomorrow', 'tomorrowTasks')
+                : taskItems.tomorrowTasks}
+            </TaskListItems>
+          </TimeLineList>
 
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">Today</p>
-            <ul ref="list" className="task-items">
-              {!taskItems.todayTasks ? this.getEmptyTask('today', 'todayTasks') : taskItems.todayTasks}
-            </ul>
-          </li>
+          <TimeLineList>
+            <TimeLinePoint />
+            <TimeLineText>This week</TimeLineText>
+            <TaskListItems>
+              {!taskItems.weekTasks
+                ? getEmptyTask('week', 'weekTasks')
+                : taskItems.weekTasks}
+            </TaskListItems>
+          </TimeLineList>
 
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">Tomorrow</p>
-            <ul ref="list" className="task-items">
-              {!taskItems.tomorrowTasks ? this.getEmptyTask('tomorrow', 'tomorrowTasks') : taskItems.tomorrowTasks}
-            </ul>
-          </li>
+          <TimeLineList>
+            <TimeLinePoint />
+            <TimeLineText>This month</TimeLineText>
+            <TaskListItems>
+              {!taskItems.monthTasks
+                ? getEmptyTask('month', 'monthTasks')
+                : taskItems.monthTasks}
+            </TaskListItems>
+          </TimeLineList>
 
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">This week</p>
-            <ul ref="list" className="task-items">
-              {!taskItems.weekTasks ? this.getEmptyTask('week', 'weekTasks') : taskItems.weekTasks}
-            </ul>
-          </li>
+          <TimeLineList>
+            <TimeLinePoint />
+            <TimeLineText>Later</TimeLineText>
+            <TaskListItems>
+              {!taskItems.laterTasks
+                ? getEmptyTask('later', 'laterTasks')
+                : taskItems.laterTasks}
+            </TaskListItems>
+          </TimeLineList>
 
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">This month</p>
-            <ul ref="list" className="task-items">
-              {!taskItems.monthTasks ? this.getEmptyTask('month', 'monthTasks') : taskItems.monthTasks}
-            </ul>
-          </li>
-
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">Later</p>
-            <ul ref="list" className="task-items">
-              {!taskItems.laterTasks ? this.getEmptyTask('later', 'laterTasks') : taskItems.laterTasks}
-            </ul>
-          </li>
-
-          <li className="time-line__list">
-            <span className="time-line__point"/>
-            <p className="time-line__text">No Due Dates</p>
-            <ul ref="list" className="task-items">
-              {!taskItems.noDueDatesTasks ? this.getEmptyTask('noDueDates', 'noDueDatesTasks') : taskItems.noDueDatesTasks}
-            </ul>
-          </li>
-        </ul>}
-      </div>
-    )
-  }
+          <TimeLineList>
+            <TimeLinePoint />
+            <TimeLineText>No Due Dates</TimeLineText>
+            <TaskListItems>
+              {!taskItems.noDueDatesTasks
+                ? getEmptyTask('noDueDates', 'noDueDatesTasks')
+                : taskItems.noDueDatesTasks}
+            </TaskListItems>
+          </TimeLineList>
+        </TimeLine>
+      )}
+    </div>
+  )
 }
 
+TaskList.propTypes = {
+  // Data
+  userId: PropTypes.string,
+  tasks: PropTypes.array,
+  listType: PropTypes.string,
+  selectedTasks: PropTypes.object,
+  selectedTags: PropTypes.object,
+  isVisibleArchivedTasks: PropTypes.bool,
+  timeLine: PropTypes.bool,
+  sort: PropTypes.object,
+  leftPanelWidth: PropTypes.number,
+  windowWidth: PropTypes.number,
+
+  // Handlers
+  onCompleteClick: PropTypes.func,
+  onTaskSelect: PropTypes.func,
+  moveTask: PropTypes.func,
+  dropTask: PropTypes.func,
+  onTagClick: PropTypes.func,
+  setArchiveTasks: PropTypes.func,
+  cancelArchiveTasks: PropTypes.func,
+  acceptTask: PropTypes.func,
+  rejectTask: PropTypes.func,
+}
+
+export default TaskList
