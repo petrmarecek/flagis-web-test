7673e26752ffc873697f153993588a793808dc26
Petr <petr.marecekk@gmail.com>
2019-01-23 11:27:26 +0100

    fix(safari): Fix position of clear for tag filter in safari

--- app/components/autocomplete/styles.js
+++ app/components/autocomplete/styles.js
@@ -6,38 +6,40 @@ import {
   placeholderColor,
   textOverflow,
   fontSub,
-  fontMain
+  fontMain,
 } from '../styled-components-mixins'
 
 // -------------------------------------- Autocompelete --------------------------------------------------
 const AutocompleteContainer = styled.ul`
   ${boxSizing('border-box')}
   margin: 0;
-  float: ${props => props.taskDetailTags ? 'right' : 'none'};
-  padding: ${props => props.taskDetailTags ? '0' : '8px 0 0'};
+  float: ${props => (props.taskDetailTags ? 'right' : 'none')};
+  padding: ${props => (props.taskDetailTags ? '0' : '8px 0 0')};
   height: auto !important;
   list-style-type: none;
   cursor: text;
   border: none;
 
   &:after {
-      content: "";
-      display: block;
-      clear: both;
+    content: '';
+    display: block;
+    clear: both;
   }
-`;
+`
 
 const Clear = styled.li`
   ${boxSizing('border-box')}
   ${borderRadius('13px')}
+  display: flex;
+  justify-content: center;
+  align-items: center;
   float: left;
   border: 1px solid #8c9da9;
-  padding: 4px 0 0 5px;
   height: 23px;
   width: 23px;
   cursor: pointer;
   margin: 0 5px 0 0;
-`;
+`
 
 const Search = styled.li`
   display: flex;
@@ -46,24 +48,24 @@ const Search = styled.li`
   border: none;
   padding: 0 5px;
   height: 25px;
-  float: ${props => props.taskDetailTags ? 'right' : 'none'};
+  float: ${props => (props.taskDetailTags ? 'right' : 'none')};
 
   &:after {
-      content: "";
-      clear: both;
+    content: '';
+    clear: both;
   }
-`;
+`
 
 // -------------------------------------- AutocompeleteInput ---------------------------------------------
 const InputContainer = styled.div`
   width: 100%;
-`;
+`
 
 const Input = styled.input`
   ${placeholderColor('#8c9da9')}
   ${fontSub}
   position: relative;
-  color: ${props => props.mainSearch ? '#fff': '#555'};
+  color: ${props => (props.mainSearch ? '#fff' : '#555')};
   background: none repeat scroll 0 0 transparent !important;
   border: 0 none !important;
   outline: 0 none;
@@ -72,7 +74,7 @@ const Input = styled.input`
   cursor: text;
   margin: 0 !important;
   padding: 1px 0 3px 0;
-  font-size: ${props => props.mainSearch ? '15px': '14px'};
+  font-size: ${props => (props.mainSearch ? '15px' : '14px')};
   width: 100%;
 
   :active {
@@ -86,7 +88,7 @@ const Input = styled.input`
   ::-ms-clear {
     display: none;
   }
-`;
+`
 
 // -------------------------------------- AutocompeleteItem -------------------------------------------
 const TagContainer = styled.li`
@@ -101,13 +103,13 @@ const TagContainer = styled.li`
   float: left;
   height: 23px;
   line-height: 23px;
-`;
+`
 
 const ContactContainer = styled.li`
   display: flex;
   align-items: center;
   padding: 0 0 6px 0;
-`;
+`
 
 const Title = styled.span`
   font-size: 14px;
@@ -117,7 +119,7 @@ const Title = styled.span`
   overflow: hidden;
   white-space: nowrap;
   ${textOverflow('ellipsis')}
-`;
+`
 
 const Delete = styled.span`
   ${boxSizing('border-box')}
@@ -128,13 +130,13 @@ const Delete = styled.span`
   cursor: pointer;
   flex-shrink: 0;
   position: relative;
-`;
+`
 
 const DeleteIcon = styled(Icon)`
   position: absolute;
   top: 3px;
   left: 3px;
-`;
+`
 
 export {
   AutocompleteContainer,
