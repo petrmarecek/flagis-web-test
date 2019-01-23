3f7acc6b981bd73d6ced2999137343f9b6c13298
Petr <petr.marecekk@gmail.com>
2019-01-23 11:01:26 +0100

    fix(explorer-and-edge): Fix problem for internet exlorer and edge

--- app/components/common/follower-status.js
+++ app/components/common/follower-status.js
@@ -6,24 +6,30 @@ import styled, { keyframes } from 'styled-components'
 import { transition, boxSizing } from 'components/styled-components-mixins'
 import { pulse } from 'react-animations'
 
-const show = keyframes`${pulse}`;
+const show = keyframes`${pulse}`
 const hide = keyframes`
     0%    {opacity: 0;}
     99%   {opacity: 0;}
     100%  {opacity: 1;}
-`;
+`
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
   position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
   margin: 0 5px;
-  animation: ${props => props.animation ? `500ms ${hide}, 500ms ${show} linear 500ms` : 'none'};
-`;
+  animation: ${props =>
+    props.animation ? `500ms ${hide}, 500ms ${show} linear 500ms` : 'none'};
+`
 
 const Button = styled.button`
   ${boxSizing('border-box')}
   display: flex;
   justify-content: center;
-  background-color: #44FFB1;
+  background-color: #44ffb1;
   width: 123px;
   border: none;
   padding: 0;
@@ -32,31 +38,36 @@ const Button = styled.button`
     ${transition('500ms')}
     background-color: #293034;
   }
-`;
+`
 
 const Title = styled.span`
   font-size: 16px;
   line-height: 26px;
   color: ${props => props.color};
-  animation: ${props => props.animation ? `500ms ${hide}, 500ms ${show} linear 500ms` : 'none'};
-`;
+  animation: ${props =>
+    props.animation ? `500ms ${hide}, 500ms ${show} linear 500ms` : 'none'};
+`
 
 const FollowerStatus = ({ status, animation, onHandleSend }) => {
   const getStatus = {
-     new: (
-       <Button onClick={onHandleSend}>
-         <Title color='#fff'>SEND TASK</Title>
-       </Button>
-     ),
-     pending: <Title color='#8C9DA9' animation={animation}>WAITING FOR RESPONSE</Title>,
-     rejected: <Title color='#FF6A6A' animation={animation}>REJECTED</Title>
+    new: (
+      <Button onClick={onHandleSend}>
+        <Title color="#fff">SEND TASK</Title>
+      </Button>
+    ),
+    pending: (
+      <Title color="#8C9DA9" animation={animation}>
+        WAITING FOR RESPONSE
+      </Title>
+    ),
+    rejected: (
+      <Title color="#FF6A6A" animation={animation}>
+        REJECTED
+      </Title>
+    ),
   }
 
-  return (
-    <Container animation={animation}>
-      {getStatus[status]}
-    </Container>
-  )
+  return <Container animation={animation}>{getStatus[status]}</Container>
 }
 
 FollowerStatus.propTypes = {
@@ -67,5 +78,5 @@ FollowerStatus.propTypes = {
 }
 
 export default withHandlers({
-  onHandleSend: props => () => props.onSend()
+  onHandleSend: props => () => props.onSend(),
 })(FollowerStatus)
