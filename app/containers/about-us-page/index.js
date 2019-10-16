e69683e55b7bc4652b6e3dbd3a0e41fc8dd37a98
petr <petr.marecekk@gmail.com>
2019-10-16 21:30:08 +0200

    feat(about-us): Add text for about us page

--- app/containers/about-us-page/index.js
+++ app/containers/about-us-page/index.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 
 // components
 import NavigationLandingPrimary from 'components/navigation/navigation-landing-primary'
-import styled from 'styled-components'
 
-const ContactUsWrapper = styled.div``
+// styles
+import styled from 'styled-components'
+import { colors } from 'components/styled-components-mixins/colors'
+
 const InnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,21 +18,74 @@ const InnerWrapper = styled.div`
 const Title = styled.div`
   font-size: 60px;
   font-weight: bold;
-  margin-bottom: 100px;
+  margin-bottom: 80px;
+`
+
+const Line = styled.div`
+  height: 2px;
+  width: 50px;
+  margin-bottom: 30px;
+  border: 2px solid ${colors.darkJungleGreen};
 `
 
 const Content = styled.div`
-  font-size: 32px;
+  p {
+    font-weight: 300;
+    font-size: 20px;
+    max-width: 768px;
+    margin-bottom: 30px;
+    text-align: justify;
+
+    :first-child {
+      font-weight: 500;
+    }
+  }
 `
 
 const ContactUsPage = ({ location }) => (
-  <ContactUsWrapper>
+  <div>
     <NavigationLandingPrimary location={location} />
     <InnerWrapper>
       <Title>About Us</Title>
-      <Content>comming soon...</Content>
+      <Content>
+        <p>It's your personal assistant for the things that matter to you!</p>
+        <Line />
+        <p>
+          Flagis is a simple universal solution for managing all the things that
+          matter to you with the help of "getting things done" principles.
+        </p>
+        <p>
+          Flagis provides one solution for managing your own tasks, and the
+          tasks you sent to and received from the others, in the same way as
+          emails.
+        </p>
+        <p>
+          It allows you to manage a single source of task items that matter to
+          you regardless of whether you created them manually or received from
+          others. You can manage them and track responsibility in a very simple
+          and efficient way.
+        </p>
+        <p>
+          Instead of non-smart sending or just assigning a task to others, there
+          is an action required from the recipient, so you know if the task was
+          accepted or rejected and when that happened.
+        </p>
+        <p>
+          This provides you with transparency in cooperation, undisputable
+          responsibility and clear tracking in a single tool.
+        </p>
+        <p>
+          Flagis also allows you to put tags on your tasks. At the same time,
+          you can create a "folder tree structure" out of those tags for
+          filtering tasks, so you will always find the tasks you need.
+        </p>
+        <p>
+          As a result, you have one transparent list of everything important
+          supported by features for easy management.
+        </p>
+      </Content>
     </InnerWrapper>
-  </ContactUsWrapper>
+  </div>
 )
 
 ContactUsPage.propTypes = {
