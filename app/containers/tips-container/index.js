53f8329bc3cf09b2a95e73f053f72bfd2b7bbcbd
Patrik Krhovský <patrikkrhovsky@gmail.com>
2020-10-15 20:58:50 +0200

    fix: Fix showing tips for a second when user is logging out

--- app/containers/tips-container/index.js
+++ app/containers/tips-container/index.js
@@ -4,25 +4,28 @@ import { connect } from 'react-redux'
 
 import { InitialTips } from 'components/tips'
 import { readTip } from 'redux/store/auth/auth.actions'
-import { getUserSettings } from 'redux/store/auth/auth.selectors'
+import { getAuth, getUserSettings } from 'redux/store/auth/auth.selectors'
 
 const TipsContainer = props => {
   const handleRead = useCallback(name => props.readTip(name), [props.readTip])
 
   return (
     <div>
-      {Boolean(!props.data.initial) && <InitialTips onClose={handleRead} />}
+      {Boolean(props.isAuthenticated && !props.data.initial) &&
+      <InitialTips onClose={handleRead} />}
     </div>
   )
 }
 
 TipsContainer.propTypes = {
   data: PropTypes.object.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
   readTip: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
   data: getUserSettings(state).tips || {},
+  isAuthenticated: getAuth(state).isAuthenticated,
 })
 
 const mapDispatchToProps = { readTip }
