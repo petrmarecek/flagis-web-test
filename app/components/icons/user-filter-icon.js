06fe435ef81f438ac8b48ae77300eb3c54564657
petr <petr.marecekk@gmail.com>
2021-02-23 19:57:41 +0100

    feat: Change a color of a contact in the tag-tree

--- app/components/icons/user-filter-icon.js
+++ app/components/icons/user-filter-icon.js
@@ -1,23 +1,35 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
+
+// styles
+import { colors } from 'components/styled-components-mixins/colors'
 
 const UserFilterIcon = props => {
   return (
     <svg
       width={18}
       height={15}
-      viewBox='0 0 18 15'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
+      viewBox="0 0 18 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M1.838 14.45c.626-1.39 1.594-2.467 2.879-3.193.899-.491 1.817-.176 2.623.102l.055.018c.5.181.979.342 1.438.342.46 0 .94-.16 1.44-.341l.055-.02c.806-.277 1.723-.592 2.602-.101 1.386.76 2.404 1.928 3.031 3.448a9 9 0 10-14.123-.254zM9 10c2.201 0 4-1.799 4-4s-1.799-4-4-4a4.01 4.01 0 00-4 4c0 2.201 1.799 4 4 4z'
-        fill='#9188E3'
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1.838 14.45c.626-1.39 1.594-2.467 2.879-3.193.899-.491 1.817-.176 2.623.102l.055.018c.5.181.979.342 1.438.342.46 0 .94-.16 1.44-.341l.055-.02c.806-.277 1.723-.592 2.602-.101 1.386.76 2.404 1.928 3.031 3.448a9 9 0 10-14.123-.254zM9 10c2.201 0 4-1.799 4-4s-1.799-4-4-4a4.01 4.01 0 00-4 4c0 2.201 1.799 4 4 4z"
+        fill={props.color}
       />
     </svg>
   )
+}
+
+UserFilterIcon.defaultProps = {
+  color: colors.astrocopusGrey,
+}
+
+UserFilterIcon.propTypes = {
+  color: PropTypes.string,
 }
 
 export default UserFilterIcon
