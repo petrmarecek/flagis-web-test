293413f9a98860786ef1f1b211e4ecbb93a29b26
petr <petr.marecekk@gmail.com>
2021-02-24 20:52:07 +0100

    feat: Remove a userdId from a sentry error message

--- app/redux/store/errors/errors.sagas.js
+++ app/redux/store/errors/errors.sagas.js
@@ -1,12 +1,7 @@
 import * as Sentry from '@sentry/browser'
 
-// redux
-import { select } from 'redux-saga/effects'
-import * as authSelectors from 'redux/store/auth/auth.selectors'
-
 export function* errorSentry(action) {
   const { err, data } = action.payload
-  const userId = yield select(state => authSelectors.getUserId(state))
 
   if (
     process.env.NODE_ENV === 'local' ||
@@ -16,16 +11,13 @@ export function* errorSentry(action) {
   } else {
     // set user and tag
     Sentry.configureScope(scope => {
-      scope.setUser({
-        id: userId,
-      })
       scope.setTag(data.tagType, data.tagValue)
     })
 
     // set breadcrumb
     Sentry.addBreadcrumb({
       category: data.breadcrumbCategory,
-      message: `USER: ${userId} => ${data.breadcrumbMessage}`,
+      message: data.breadcrumbMessage,
       level: Sentry.Severity.Info,
     })
 
